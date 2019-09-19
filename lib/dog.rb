1562d9b91519e19240a6a3f097684a54b83e626a
SarahDrake01 <jussaradrake@outlook.com>
2019-09-19 14:47:25 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,38 @@
 class Dog
-    attr_accessor :name
-  
-    @@all = []
-  
-    def initialize(name)
-      @name = name
-      #@@all << self
-      save
-    end
-  
-    def self.all
-      @@all
-    end
-  
-    def self.print_all
+  attr_accessor :name
+#create a class variable @@all se to any empty array
+#inside your class. This arrau is the storage container
+#for each instance of a Dog.
+@@all = []
 
-      puts @@all.map{ |dog| dog.name }
-    end
-  
-    def self.clear_all
-      @@all.clear
-    end
-  
-    def save
-      @@all << self
-    end
+def initialize(name)
+  @name = name
+  #@@all << self
+  save
+end
+
+#Write a classe method .all that reads this variables
+#Frojm inside the Dogs class
+def self.all
+  @@all
+end
+
+#Write a class method .print_all, that iterates over all of
+#the individual dogs stored in the @@all variable inside
+#of initialize.
+def self.print_all
+  puts all.map { |dog| dog.name }
   end
+
+  #Write a class .clear_all this method should operate
+  #on the @@all array of existing dogs and empty that array.
+def self.clear_all
+  @@all.clear
+end
+
+#Write a method save the method should handle the task of pushing
+#self into @@all.
+def save
+  @@all << self
+  end 
+end
